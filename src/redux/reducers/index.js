6e4474f0be41eab3a50f9b066e3527451cfc7213
Reza_Ganjei <ganjeireza@gmail.com>
2023-03-13 17:09:41 +0330

    orders handler reducer created

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,19 +1,21 @@
 import { combineReducers } from "redux";
 import adminAuthReducer from "./adminAuth/adminAuth";
 import checkoutCartReducer from "./checkoutCart/checkoutCart";
+import ordersHandlerReducer from "./ordersHandler/ordersHandler";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const combinedReducers = combineReducers({
   adminAuth: adminAuthReducer,
   checkoutCart: checkoutCartReducer,
+  ordersHandler: ordersHandlerReducer,
 });
 
 const persistedReducers = persistReducer(
   {
     key: "rootPersistor",
     storage,
-    whitelist: ["adminAuth", "checkoutCart"],
+    whitelist: ["adminAuth", "checkoutCart", "ordersHandler"],
   },
   combinedReducers
 );
